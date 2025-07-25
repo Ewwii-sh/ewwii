bf61dd030a8ff077b17e22aae94450a7ea46c354
Byson94 <byson94@outlook.com>
2025-07-25 19:07:34 +0530

    chore: replaced iirhai with the latest code from rhai-dlt

---
+++ crates/iirhai/src/widgetnode.rs
@@ -0,0 +1,29 @@
+use rhai::Map;
+
+#[derive(Debug, Clone)]
+pub enum WidgetNode {
+    Label(String),
+    Box { props: Map, children: Vec<WidgetNode> },
+    CenterBox { props: Map, children: Vec<WidgetNode> },
+    Button { props: Map, children: Vec<WidgetNode> },
+    Image { props: Map },
+    Input { props: Map },
+    Progress { props: Map },
+    Spacer { props: Map },
+    Slider { props: Map },
+    Revealer { props: Map, children: Vec<WidgetNode> },
+    Scroll { props: Map, children: Vec<WidgetNode> },
+    Calendar { props: Map },
+    Graph { props: Map },
+    Include(String),
+    DefStyle(String),
+    EventBox { props: Map, children: Vec<WidgetNode> },
+
+    // Top-level macros
+    DefWindow { name: String, props: Map, node: Box<WidgetNode> },
+    // Poll { var: String, interval: String, cmd: String, initial: String },
+    // Listen { var: String, signal: String },
+    Poll { var: String, props: Map },
+    Listen { var: String, props: Map },
+    Enter(Vec<WidgetNode>),
+}
--- crates/iirhaixpr/src/widgetnode.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, Clone)]
-pub enum WidgetNode {
-    Label(Spanned<String>),
-    Row(Spanned<Vec<WidgetNode>>),
-    Box { dir: Spanned<String>, children: Spanned<Vec<WidgetNode>> },
-}
