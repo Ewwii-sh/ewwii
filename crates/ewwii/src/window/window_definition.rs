c4e869256481d84e629f8f2d62fa85cf11e685c9
Byson94 <byson94@outlook.com>
2025-07-29 22:46:21 +0530

    fix: Fixed compilation errors on WindowStacking

---
+++ crates/ewwii/src/window/window_definition.rs
@@ -0,0 +1,35 @@
+use crate::enum_parse;
+use anyhow::anyhow;
+
+#[derive(Debug, thiserror::Error)]
+pub struct EnumParseError {
+    pub input: String,
+    pub expected: Vec<&'static str>,
+}
+impl Display for EnumParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Failed to parse `{}`, must be one of {}", self.input, self.expected.join(", "))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, derive_more::Display, smart_default::SmartDefault, serde::Serialize)]
+pub enum WindowStacking {
+    #[default]
+    Foreground,
+    Background,
+    Bottom,
+    Overlay,
+}
+
+impl std::str::FromStr for WindowStacking {
+    type Err = EnumParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        enum_parse! { "WindowStacking", s,
+            "foreground" | "fg" => WindowStacking::Foreground,
+            "background" | "bg" => WindowStacking::Background,
+            "bottom" | "bt" => WindowStacking::Bottom,
+            "overlay" | "ov" => WindowStacking::Overlay,
+        }
+    }
+}
