00a4561a4b5500bf3c87db5c3581ce05f004f286
Byson94 <byson94@outlook.com>
2025-07-30 20:42:34 +0530

    feat: added widget_definitions_helper file to help widget_definition codes

---
+++ crates/ewwii/src/widgets/widget_definitions_helper.rs
@@ -0,0 +1,9 @@
+use anyhow::{Result, anyhow};
+
+pub(super) fn parse_orientation(ori: &str) -> Result<gtk::Orientation> {
+    match ori.to_ascii_lowercase().as_str() {
+        "h" | "horizontal" => Ok(gtk::Orientation::Horizontal),
+        "v" | "vertical" => Ok(gtk::Orientation::Vertical),
+        other => Err(anyhow!("Invalid orientation: {}", other)),
+    }
+}
