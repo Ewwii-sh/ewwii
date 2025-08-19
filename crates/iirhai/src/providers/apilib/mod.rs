712172b43433b53bc2e4276abb629e432182f591
Byson94 <byson94@outlook.com>
2025-08-19 10:26:59 +0530

    feat: added api module to rhai

---
+++ crates/iirhai/src/providers/apilib/mod.rs
@@ -0,0 +1,24 @@
+pub mod wifi;
+
+use crate::module_resolver::{ChainedResolver, SimpleFileResolver};
+use rhai::module_resolvers::StaticModuleResolver;
+use rhai::{exported_module, Engine};
+
+pub fn register_apilib(engine: &mut Engine) {
+    use crate::providers::apilib::{
+        wifi::wifi,
+    };
+
+    let mut resolver = StaticModuleResolver::new();
+
+    // adding modules
+    let wifi_mod = exported_module!(wifi);
+
+    // inserting modules
+    resolver.insert("api::wifi", wifi_mod);
+
+    let chained = ChainedResolver { first: SimpleFileResolver, second: resolver.clone() };
+
+    // Register the resolver
+    engine.set_module_resolver(chained);
+}
