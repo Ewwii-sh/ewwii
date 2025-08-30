a0a268cc88337f0ba4b867d2e911b68127dcf3ea
Byson94 <byson94@outlook.com>
2025-08-30 21:38:01 +0530

    feat: added feature for adding descriptions in generated mod docs

--- tools/generate-rhai-docs/src/main.rs
+++ tools/generate-rhai-docs/src/main.rs
@@ -3,7 +3,7 @@ use rhai::{Engine, module_resolvers::StaticModuleResolver};
 use rhai_autodocs::{export::options, generate::mdbook};
 use std::{env, fs, path::Path};
 
-fn generate_docs(engine: &Engine, path: &str, filename: &str, include_std: bool) {
+fn generate_docs(engine: &Engine, path: &str, filename: &str, include_std: bool, pre_description: &str) {
     let docs = options()
         .include_standard_packages(include_std)
         .export(engine)
@@ -21,9 +21,12 @@ fn generate_docs(engine: &Engine, path: &str, filename: &str, include_std: bool)
     let full_docs =
         docs_content.into_iter().map(|(_, doc)| doc).collect::<Vec<String>>().join("\n");
 
+    // combination of all docs and pre description
+    let final_docs = format!("{}\n\n{}", pre_description, full_docs);
+
     // Write documentation to markdown file
     let file_path = Path::new(path).join(format!("{}.md", filename));
-    fs::write(&file_path, full_docs).expect("failed to write documentation");
+    fs::write(&file_path, final_docs).expect("failed to write documentation");
     println!("Documentation generated at: {}", file_path.display());
 }
 
@@ -36,7 +39,19 @@ fn main() {
     let mut resolver = StaticModuleResolver::new();
 
     // Generate global.md (full docs)
-    generate_docs(&engine, path, "global", true);
+    generate_docs(&engine, path, "global", true, r#"
+        # Global Builtin Rhai Functions
+
+        These functions are built-in and available globally, meaning they can be used directly without any import.
+
+        For example, to get the value of PI, you can simply write:
+        
+        ```js
+        let x = PI();
+        ```
+
+        This section covers all the core functions provided by Rhai that are ready to use out of the box.
+    "#);
 
     // Generate stdlib.md docs (custom stdlib)
     resolver.clear();
@@ -45,7 +60,15 @@ fn main() {
     for (module_name, module) in resolver.iter() {
         engine.register_static_module(module_name, module.clone());
     }
-    generate_docs(&engine, path, "stdlib", false);
+    generate_docs(&engine, path, "stdlib", false, r#"
+        # Std Library Module
+
+        These are all the standard modules in ewwii.
+
+        Each library in this module is under `std::<m>`, where `<m>` is the name of the specific module.
+        These modules provide essential functionalities that are will be useful for making widgets.
+        They cover tasks like string manipulation, environmental variable manipuation, running shell commands, and more.
+    "#);
 
     // Generate apilib.md docs
     resolver.clear();
@@ -54,7 +77,15 @@ fn main() {
     for (module_name, module) in resolver.iter() {
         engine.register_static_module(module_name, module.clone());
     }
-    generate_docs(&engine, path, "apilib", false);
+    generate_docs(&engine, path, "apilib", false, r#"
+        # API Library Module
+
+        These are all the API modules available in ewwii.
+
+        Each library in this module is under `api::<m>`, where `<m>` is the name of the specific module.
+        
+        The API library provides system-level functionality, allowing you to interact with external resources and perform advanced operations. Examples include interacting with Wi-Fi, networking, and more.
+    "#);
 
     println!("Docs generation completed.");
 }
