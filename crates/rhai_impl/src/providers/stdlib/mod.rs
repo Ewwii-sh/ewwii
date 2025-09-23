25ed97cc535b4ce852783fe59de6bcc2a0e92883
Byson94 <byson94@outlook.com>
2025-09-23 20:00:21 +0530

    feat(stdlib): added std::regex for regex matching

--- crates/rhai_impl/src/providers/stdlib/mod.rs
+++ crates/rhai_impl/src/providers/stdlib/mod.rs
@@ -1,23 +1,28 @@
 pub mod command;
 pub mod env;
 pub mod monitor;
+pub mod regex;
 pub mod text;
 
 use rhai::exported_module;
 use rhai::module_resolvers::StaticModuleResolver;
 
 pub fn register_stdlib(resolver: &mut StaticModuleResolver) {
-    use crate::providers::stdlib::{command::command, env::env, monitor::monitor, text::text};
+    use crate::providers::stdlib::{
+        command::command, env::env, monitor::monitor, regex::regex_lib, text::text,
+    };
 
     // adding modules
     let text_mod = exported_module!(text);
     let env_mod = exported_module!(env);
     let monitor_mod = exported_module!(monitor);
     let command_mod = exported_module!(command);
+    let regex_mod = exported_module!(regex_lib);
 
     // inserting modules
     resolver.insert("std::text", text_mod);
     resolver.insert("std::env", env_mod);
     resolver.insert("std::monitor", monitor_mod);
     resolver.insert("std::command", command_mod);
+    resolver.insert("std::regex", regex_mod);
 }
