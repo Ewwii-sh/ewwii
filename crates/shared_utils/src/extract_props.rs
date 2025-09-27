0094818d73c322b1db51182c00615eaedf82ce3b
Byson94 <byson94@outlook.com>
2025-09-27 11:49:59 +0530

    fix: m/min duration end & ewwii crash

--- crates/shared_utils/src/extract_props.rs
+++ crates/shared_utils/src/extract_props.rs
@@ -116,20 +116,21 @@ pub fn get_duration_prop(props: &Map, key: &str, default: Option<Duration>) -> R
             Ok(Duration::from_secs(s))
         } else if key_str.ends_with("min") {
             let num = &key_str[..key_str.len() - 3];
-            let mins = num.parse::<u64>()
-                .map_err(|_| anyhow!("Invalid min value: '{}'", key_str))?;
+            let mins =
+                num.parse::<u64>().map_err(|_| anyhow!("Invalid min value: '{}'", key_str))?;
             Ok(Duration::from_secs(mins * 60))
         } else if key_str.ends_with("m") {
             let num = &key_str[..key_str.len() - 1];
-            let mins = num.parse::<u64>()
-                .map_err(|_| anyhow!("Invalid m value: '{}'", key_str))?;
+            let mins = num.parse::<u64>().map_err(|_| anyhow!("Invalid m value: '{}'", key_str))?;
             Ok(Duration::from_secs(mins * 60))
         } else if key_str.ends_with("h") {
             let num = &key_str[..key_str.len() - 1];
             let hrs = num.parse::<u64>().map_err(|_| anyhow!("Invalid h value: '{}'", key_str))?;
             Ok(Duration::from_secs(hrs * 3600))
         } else {
-            Err(anyhow!("Unsupported duration format: '{}'", key_str))
+            default.ok_or_else(|| {
+                anyhow!("Unsupported duration format: '{}', and no default provided", key_str)
+            })
         }
     } else {
         default.ok_or_else(|| anyhow!("No value for duration and no default provided"))
