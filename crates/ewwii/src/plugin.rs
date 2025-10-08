c999b53691e5b08ce5599e2b3f6ff492f91cdda9
Byson94 <byson94@outlook.com>
2025-10-08 18:06:34 +0530

    feat: getting rhai mod working via plugin

--- crates/ewwii/src/plugin.rs
+++ crates/ewwii/src/plugin.rs
@@ -1,13 +1,26 @@
 use ewwii_plugin_api::EwwiiAPI;
 use rhai::Engine;
-use rhai_impl::parser::ParseConfig;
-use std::sync::{Arc, RwLock};
+use std::sync::mpsc::{Sender};
 
-pub struct EwwiiImpl;
+pub(crate) struct EwwiiImpl {
+    pub(crate) requestor: Sender<PluginRequest>,
+}
 
 impl EwwiiAPI for EwwiiImpl {
     // General
     fn log(&self, msg: &str) {
         println!("[HOST LOG] {}", msg);
     }
+
+    // Rhai Manipulation Stuff
+    fn rhai_engine_action(&self, f: Box<dyn FnOnce(&mut Engine) + Send>) -> Result<(), String> {
+        self.requestor
+            .send(PluginRequest::SetRhaiEngine(f))
+            .map_err(|_| "Failed to send request to host".to_string())?;
+        Ok(())
+    }
+}
+
+pub(crate) enum PluginRequest {
+    RhaiEngineAct(Box<dyn FnOnce(&mut Engine) + Send>),
 }
