425ef5020be3e5666de185de6938dbb1ea8ec1a8
Byson94 <byson94@outlook.com>
2025-10-30 14:12:10 +0530

    feat: set gtk box space evenly to true

--- crates/ewwii/src/widgets/widget_definitions.rs
+++ crates/ewwii/src/widgets/widget_definitions.rs
@@ -269,7 +269,7 @@ pub(super) fn build_gtk_box(
         handle_signal_or_value(
             &props,
             "space_evenly",
-            |p, k| get_bool_prop(p, k, None),
+            |p, k| get_bool_prop(p, k, Some(true)),
             |signal| {
                 let widget = widget.clone();
                 let signal_widget = signal.data;
